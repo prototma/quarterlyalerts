41c19b3b3846e2031b95ceeff84f484b00c02c37
prototma <prototma@gmail.com>
2025-08-01 07:50:53 +0630

    Create BootCompletedReceiver.kt

---
+++ app/src/main/java/com/example/quarterlyalerts/BootCompletedReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.quarterlyalerts
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            // TODO: reschedule alerts or restart service after reboot
+        }
+    }
+}
